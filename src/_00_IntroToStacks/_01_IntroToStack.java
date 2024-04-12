0ebea645406bf80a5d862cac27d2392cce3de0bb
league <57720048+richardson-liou@users.noreply.github.com>
2024-04-11 19:58:35 -0700

    Finished Intro to Stacks and Text Undo

--- src/_00_IntroToStacks/_01_IntroToStack.java
+++ src/_00_IntroToStacks/_01_IntroToStack.java
@@ -9,13 +9,29 @@ public class _01_IntroToStack {
     public static void main(String[] args) {
         // 1. Create a Stack of Doubles
         //    Don't forget to import the Stack class
+    	Stack<Double> nums = new Stack<Double>();
 
         // 2. Use a loop to push 100 random doubles between 0 and 100 to the Stack.
+    	Random random = new Random();
+    	for (int i = 0; i < 100; i++)
+    	{
+    		double rand = random.nextDouble() * 100;
+    		nums.push(rand);
+    	}
 
         // 3. Ask the user to enter in two numbers between 0 and 100, inclusive. 
-
+    	double number1 = Double.parseDouble(JOptionPane.showInputDialog("Enter a number from 0 to 100"));
+    	double number2 = Double.parseDouble(JOptionPane.showInputDialog("Enter another number from 0 to 100"));
         // 4. Pop all the elements off of the Stack. Every time a double is popped that is
         //    between the two numbers entered by the user, print it to the screen.
+    	for (int i = 0; i < nums.size(); i++)
+    	{
+    		double poppedNumber = nums.pop();
+    		if ((poppedNumber >= number1 && poppedNumber <= number2) || (poppedNumber >= number2 && poppedNumber <= number1))
+    		{
+    			System.out.println(poppedNumber);
+    		}
+    	}
 
 
         // EXAMPLE:
--- src/_00_IntroToStacks/_02_TextUndoRedo.java
+++ src/_00_IntroToStacks/_02_TextUndoRedo.java
@@ -1,6 +1,15 @@
 package _00_IntroToStacks;
 
-public class _02_TextUndoRedo {
+import java.awt.Dimension;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.Stack;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class _02_TextUndoRedo implements KeyListener {
     /* 
      * Create a JFrame with a JPanel and a JLabel.
      * 
@@ -17,5 +26,66 @@ public class _02_TextUndoRedo {
      * the JLabel.
      */
 
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JLabel label = new JLabel();
+	
+	String text = "";
+	
+	Stack<Character> textStack = new Stack<Character>();
+	Stack<Character> recentlyDeleted = new Stack<Character>(); 
+	
+	public static void main(String[] args) {
+		new _02_TextUndoRedo().run();
+	}
+	
+	public void run()
+	{
+		frame.add(panel);
+		panel.add(label);
+		frame.setVisible(true);
+		frame.addKeyListener(this);
+		frame.setPreferredSize(new Dimension(400, 300));
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.pack();
+	}
 
+	@Override
+	public void keyPressed(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		System.out.println(arg0.getKeyCode());
+		if (arg0.getKeyCode() == 8)
+		{
+			if (text.length() > 0)
+			{
+				recentlyDeleted.push(text.charAt(text.length() - 1));
+				text = text.substring(0, text.length() - 1);
+				label.setText(text);
+			}
+		}
+		else if (arg0.getKeyCode() == 157)
+		{
+			if (recentlyDeleted.size() > 0)
+			{
+				text = text + recentlyDeleted.pop();
+				label.setText(text);
+			}
+		}
+		else if (Character.isDefined(arg0.getKeyChar()))
+		{
+			text = text + arg0.getKeyChar();
+			label.setText(text);
+		}
+	}
+
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+	}
 }
