2ae0e47bbc2068dc798926015d83f7c285d68924
league <league@192.168.1.82>
2024-05-02 19:59:40 -0700

    Finished Hangman and maze class, working on MazeMaker.java

--- src/_03_Hangman/Hangman.java
+++ src/_03_Hangman/Hangman.java
@@ -1,9 +1,12 @@
 package _03_Hangman;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.Stack;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
@@ -12,6 +15,7 @@ import javax.swing.JPanel;
 public class Hangman implements KeyListener {
 	JFrame frame;
 	JPanel panel;
+	JLabel progress;
 	JLabel label;
 	JLabel guessedLetters;
 	
@@ -21,6 +25,9 @@ public class Hangman implements KeyListener {
 	Stack<Character> guessedList;
 	Stack<Character> wrongList;
 	Boolean gameStart;
+	
+	int round;
+	int totalRounds;
 	
 	public static void main(String[] args) {
 		new Hangman().run();
@@ -32,7 +39,22 @@ public class Hangman implements KeyListener {
 		int rounds = 0;
 		while (rounds < 1 || rounds > Utilities.getTotalWordsInFile("dictionary.txt"))
 		{
-			rounds = Integer.parseInt(JOptionPane.showInputDialog("Input the number of rounds (1 -" + Utilities.getTotalWordsInFile("dictionary.txt") + ")")); 
+			String input = JOptionPane.showInputDialog("Input the number of rounds (1 -" + Utilities.getTotalWordsInFile("dictionary.txt") + ")"); 
+			try
+			{
+				rounds = Integer.parseInt(input);
+			}
+			catch (final NumberFormatException e)
+			{
+				if (input == null)
+				{
+					System.exit(0);
+				}
+				else
+				{
+					continue;			
+				}
+			}
 		}
 		
 		stack = new Stack<String>();
@@ -49,12 +71,17 @@ public class Hangman implements KeyListener {
 			}
 			stack.push(selectedword);
 		}
+		round = 0;
+		totalRounds = stack.size();
+		
 		
 		frame = new JFrame();
 		panel = new JPanel();
 		frame.add(panel);
+		progress = new JLabel();
 		label = new JLabel();
 		guessedLetters = new JLabel();
+		panel.add(progress);
 		panel.add(label);
 		panel.add(guessedLetters);
 		frame.addKeyListener(this);
@@ -67,10 +94,19 @@ public class Hangman implements KeyListener {
 	
 	void startRound()
 	{
+		round = totalRounds - stack.size();
+		if (stack.size() == 0)
+		{
+			gameStart = false;
+			JOptionPane.showMessageDialog(null, "You Win");
+			frame.dispose();
+			run();
+		}
 		guessedList = new Stack<Character>();
 		wrongList = new Stack<Character>();
 		lives = 10;
 		word = stack.pop();
+		progress.setText(round + "/" + totalRounds);
 		gameStart = true;
 		update();
 	}
@@ -89,7 +125,7 @@ public class Hangman implements KeyListener {
 				placeholder += "_";
 			}
 		}
-		label.setText("Word: " + placeholder);
+		label.setText("Word: " + placeholder + " Lives: " + lives);
 		
 		String wrongLetters = "";
 		for (int i = 0; i < wrongList.size(); i++)
@@ -98,6 +134,13 @@ public class Hangman implements KeyListener {
 		}
 		guessedLetters.setText("Guessed Letters:" + wrongLetters);
 		frame.pack();
+		if (lives == 0)
+		{
+			gameStart = false;
+			JOptionPane.showMessageDialog(null, "Game Over\nThe word was: " + word);
+			frame.dispose();
+			run();
+		}
 		checkWin();
 	}
 
@@ -137,6 +180,7 @@ public class Hangman implements KeyListener {
 			else
 			{
 				wrongList.add(arg0.getKeyChar());
+				lives--;
 			}
 			update();
 		}
